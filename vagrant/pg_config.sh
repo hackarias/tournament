357271c07e5ebe9e53b53cf1bbb2b391b3105fc9
Zack <zack.gustavsson@gmail.com>
2015-06-07 20:23:26 +0200

    Now running unit tests when default vagrant VM has been setup

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -10,6 +10,7 @@ su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb tournament'
 su vagrant -c 'psql -f /vagrant/tournament/tournament.sql'
+su vagrant -c 'python /vagrant/tournament/tournament_test.py'
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
